9b3bfc1da4feadd3ba7b83b55d3bd5d51ec568c7
Harsh Sharma <hs804506@gmail.com>
2022-05-08 00:40:09 +0530

    First Commit

---
+++ 303. Range Sum Query - Immutable.cpp
@@ -0,0 +1,15 @@
+class NumArray {
+public:
+    vector<int> v;
+    NumArray(vector<int>& nums) {
+        v = nums;
+    }
+    
+    int sumRange(int left, int right) {
+        int sum=0;
+        for(int i=left;i<=right;i++){
+            sum += v[i];
+        }
+        return sum;
+    }
+};
