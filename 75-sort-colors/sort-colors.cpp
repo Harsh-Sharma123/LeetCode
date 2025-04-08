51fcfcb2d40064692c858dd6cbeb210d279af68f
Harsh Sharma <hs804506@gmail.com>
2025-04-09 00:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (96.11%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        int zeroes = 0, ones = 0 , twos = 0;
+        for(auto i: nums){
+            if(i == 0) zeroes++;
+            else if(i==1) ones++;
+            else twos++;
+        }
+        cout << zeroes << " " << ones << " " << twos << endl;
+        for(int i=0;i<nums.size();i++){
+            if(i<zeroes){
+                nums[i] = 0;
+            }else if(i>=zeroes && i<ones+zeroes){
+                nums[i] = 1;
+            }else  nums[i] = 2;
+        }
     }
 };
